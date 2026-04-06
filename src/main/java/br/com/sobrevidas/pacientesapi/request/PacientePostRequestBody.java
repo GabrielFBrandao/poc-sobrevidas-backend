b8fc4ea2594bcb5f2bd98909dc168345b77de01a
Gabriel Brandao <bielbrandao11@gmail.com>
2026-04-06 19:27:19 -0300

    adiciona DTOs PacientePostRequestBody e PacientePutRequestBody

---
+++ src/main/java/br/com/sobrevidas/pacientesapi/request/PacientePostRequestBody.java
@@ -0,0 +1,29 @@
+package br.com.sobrevidas.pacientesapi.request;
+
+import lombok.Data;
+import java.time.LocalDate;
+
+@Data
+public class PacientePostRequestBody {
+
+    private String nome;
+    private String cpf;
+    private LocalDate dataNascimento;
+    private String sexo;
+    private String email;
+    private String telefoneCelular;
+    private String telefoneResponsavel;
+    private String nomeMae;
+    private String numCartaoSus;
+    private String cep;
+    private String endereco;
+    private String numEndereco;
+    private String complemento;
+    private String bairro;
+    private String cidade;
+    private String estado;
+    private Boolean ehTabagista;
+    private Boolean ehEtilista;
+    private Boolean temLesaoSuspeita;
+    private Boolean participaSmartMonitor;
+}
