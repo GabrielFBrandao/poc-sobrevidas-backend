d29788dfed41a005367854986d741acc7b36ad6f
Gabriel Brandao <bielbrandao11@gmail.com>
2026-04-07 02:14:02 -0300

    atualiza ApplicationTests para usar Testcontainers

--- src/test/java/br/com/sobrevidas/pacientesapi/PacientesApiApplicationTests.java
+++ src/test/java/br/com/sobrevidas/pacientesapi/PacientesApiApplicationTests.java
@@ -2,12 +2,33 @@ package br.com.sobrevidas.pacientesapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class PacientesApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Container
+    static PostgreSQLContainer<?> postgres =
+            new PostgreSQLContainer<>("postgres:16")
+                    .withDatabaseName("testdb")
+                    .withUsername("test")
+                    .withPassword("test");
 
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+        registry.add("spring.security.oauth2.resourceserver.jwt.issuer-uri",
+                () -> "http://dummy-issuer");
+    }
+
+    @Test
+    void contextLoads() {
+    }
 }
